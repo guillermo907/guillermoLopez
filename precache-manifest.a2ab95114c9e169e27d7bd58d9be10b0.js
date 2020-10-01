4259ecf3ed2476431b357ef607c617cb343c5b53
Guillermo Lòpez Garcìa <willy@MacBook-Pro-de-Guillermo.local>
2020-10-01 17:15:34 -0500

    Updates

---
+++ precache-manifest.a2ab95114c9e169e27d7bd58d9be10b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeec6bc9b8e8d8d477852020b968a226",
+    "url": "/http//:guillermo907.github.io/guillermoLopez/index.html"
+  },
+  {
+    "revision": "7c45b416ff97ddd660d4",
+    "url": "/http//:guillermo907.github.io/guillermoLopez/static/css/main.11f1d7d3.chunk.css"
+  },
+  {
+    "revision": "e569a68c8c3e58181840",
+    "url": "/http//:guillermo907.github.io/guillermoLopez/static/js/2.9bc6f082.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/http//:guillermo907.github.io/guillermoLopez/static/js/2.9bc6f082.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c45b416ff97ddd660d4",
+    "url": "/http//:guillermo907.github.io/guillermoLopez/static/js/main.56f113b9.chunk.js"
+  },
+  {
+    "revision": "907da2efb89471125af9",
+    "url": "/http//:guillermo907.github.io/guillermoLopez/static/js/runtime-main.31538d6e.js"
+  }
+]);
