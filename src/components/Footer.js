79afe2c0c2a1288faf0bcd9a218f67c9c0501fec
Guillermo Lòpez Garcìa <willy@MacBook-Pro-de-Guillermo.local>
2020-10-01 21:40:21 -0500

    cambios

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,19 @@ import React from 'react';
 
 const Footer = () =>{
     return (
-        <div>
-            Footer b
+        <div className="ui three item inverted menu footerContainer">
+            <a class="item">
+                Guillermo López G.
+            </a>
+            <a class="item">
+                <i class="facebook icon large" />
+                <i class="twitter icon large" />
+                <i class="google icon large" />
+                <i class="youtube icon large" />
+            </a>
+            <a class="item">
+                Contacto al 331219918
+            </a>
         </div>
     );
 }
